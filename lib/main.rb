fc7e1ab43fcd78fd0daf37a544476876b9ea6dbc
Jamie Shiell <jamie@infernus.org>
2018-10-04 15:54:54 +0100

    Slowed down script due to rate limits; added dummy methods for actual archive

--- lib/main.rb
+++ lib/main.rb
@@ -15,6 +15,8 @@ class SlackChannelArchiver
     def archive_channels_unused_for_days(number_of_days)
         unarchived_public_channels = @client.channels_list(exclude_archived: true).channels
 
+        archived_count = 0
+
         unarchived_public_channels.each do |current_channel|
             channel_info = @client.channels_info(channel: current_channel.id)
             channel_created = Time.at(channel_info.channel.created)
@@ -22,15 +24,27 @@ class SlackChannelArchiver
                 last_messages = @client.channels_history(channel: current_channel.id, count: 1)
                 if last_messages.messages.empty?
                     puts "! Channel #{current_channel.name} is older than #{number_of_days} and has no messages"
+                    # @client.chat_postMessage(channel: current_channel.id, text: "This channel has had no new messages in #{number_of_days} and will hence be archived - any queries, please see #slack-admin")
+                    # @client.channels_archive(channel: current_channel.id)
+                    archived_count += 1
+
                 elsif days_ago(last_messages.messages.first.ts.to_d) > number_of_days
                     puts "! Channel #{current_channel.name} is older than #{number_of_days} days and has had no messages in at least #{number_of_days} days"
+                    # @client.chat_postMessage(channel: current_channel.id, text: "This channel is older than #{number_of_days} days and has no messages, and will hence be archived - any queries, please see #slack-admin")
+                    # @client.channels_archive(channel: current_channel.id)
+                    archived_count += 1
                 else
                     puts "o Channel #{current_channel.name} is in regular use"
                 end
             else
                 puts "o Channel #{current_channel.name} is newer than #{number_of_days} days"
             end
+
+            # channels_history & channels_info are tier 3 methods, so we needs to limit acess to ~50 a second - https://api.slack.com/docs/rate-limits#tier_t3
+            sleep(1)
         end
+
+        archived_count
     end
 
     def days_ago(time)
@@ -50,9 +64,12 @@ class Launcher
         end
 
         puts "Channels that have existed but have had no new messages for at least #{number_of_days} days will be archived"
+        puts "Please note only one channel will be checked per second due to Slack API rate limits"
 
         slack_channel_archiver = SlackChannelArchiver.new(api_token)
-        slack_channel_archiver.archive_channels_unused_for_days(number_of_days)
+        archived_count = slack_channel_archiver.archive_channels_unused_for_days(number_of_days)
+
+        puts "#{archived_count} channels were archived"
     end
 
     private
