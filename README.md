3cbae9ffba8657782da35a6a57dde51f5c0850a1
Jamie Shiell <jamie@infernus.org>
2018-10-04 14:56:12 +0100

    Added API token

--- README.md
+++ README.md
@@ -14,4 +14,11 @@ Then run:
     bin/slack-channel-archiver
 
 On the first run it'll install dependencies, which may be a bit noisy.
+
+You'll need an API token  to run it. This can be specified in three ways, in order of priority:
+
+1. Pass it as the first argument, e.g. `bin/slack-channel-archiver api-token`
+1. Set it as env var `SLACK_API_TOKEN`, e.g. `SLACK_API_TOKEN=api-token bin/slack-channel-archiver`
+1. Set in `~/.slack-channel-archiver`, e.g. `api-token: an-api-token`
     
+You can obtain a new API token by [creating a new Bot Integration](https://my.slack.com/services/new/bot) in Slack.
--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,40 @@
-def main
-    puts "Hello world"
+require 'yaml'
+
+class SlackChannelArchiver
+
+    def initialize(api_token)
+        puts "API token is #{api_token}"
+    end
+
 end
 
-main if __FILE__ == $0
+class Launcher
+
+    def initialize(args)
+        api_token = read_api_token(args)
+        if api_token.nil?
+            $stderr.puts("Usage: $0 [api-token]")
+            $stderr.puts("Alternately you may specify the API token in ~/.slack-channel-archiver as 'api-token', or via env car SLACK_API_TOKEN")
+        end
+
+        slack_channel_archiver = SlackChannelArchiver.new(api_token)
+    end
+
+    private
+
+    def read_api_token(args)
+        api_token = args.first
+
+        api_token = ENV['SLACK_API_TOKEN'] if api_token.nil?
+
+        if api_token.nil? && File.exists?("#{Dir.home}/.slack-channel-archiver")
+            config = YAML.load_file("#{Dir.home}/.slack-channel-archiver")
+            api_token = config['api-token']
+        end
+
+        api_token
+    end
+
+end
+
+Launcher.new(ARGV) if __FILE__ == $0
